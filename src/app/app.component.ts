c0abaf68746206cf95568322e717f91d06308526
fromHP <a.marleku@gmail.com>
2017-11-01 01:58:16 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'SASAv2';
+  myData = {
+  	'Companies': {},
+  	'Users': {},
+  	'Products': {},
+  	'Posts': {}
+  };
+  private apiURL = 'http://localhost:25430/api/';
+  data: any = {};
+  constructor(private http: Http){
+  	console.log('testing');
+  	this.getData('Companies');
+  	this.getInfo('Companies');
+  	this.getData('Users');
+  	this.getInfo('Users');
+  	this.getData('Products');
+  	this.getInfo('Products');
+  	this.getData('Posts');
+  	this.getInfo('Posts');
+  }
+  getData(var1){
+  	return this.http.get(this.apiURL + var1)
+  		.map((res: Response) => res.json())
+  }
+
+  getInfo(var1){
+  	this.getData(var1).subscribe(data => {
+		switch(var1) {
+		    case 'Companies':
+		        this.myData.Companies = data;
+		        break;
+		    case 'Users':
+		        this.myData.Users = data;
+		        break;
+		    case 'Products':
+		        this.myData.Products = data;
+		        break;
+		    case 'Posts':
+		        this.myData.Posts = data;
+		        break;
+		    default:
+		        this.myData.Companies = data;
+		}
+  		console.log(this.myData);
+  	})
+  }
 }
